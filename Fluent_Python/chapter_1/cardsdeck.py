85b464d4166f0531e76ed6e6d4a713b468a89c69
Maksonio <32421746+iBearchic@users.noreply.github.com>
2024-04-04 19:05:51 +0300

    Create cardsdeck.py

---
+++ Fluent_Python/chapter_1/cardsdeck.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+
+# namedtuple отлично подходит для создания простых классов, где не требуется определение методов
+Card = namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        # использование list-comprehension здесь выглядит уместно и лаконично
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        # наглядно показана суть dunder-getitem
+        return self._cards[position]
