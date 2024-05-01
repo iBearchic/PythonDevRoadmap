88506a8cb476110f310c9d3acd2de0c4a3f80d77
Maksonio <maikopm1901@yandex.ru>
2024-05-01 21:30:28 +0300

    database.py configs created

--- fastapiprj/main.py
+++ fastapiprj/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from typing import Annotated, Optional
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"data": "Hello World"}
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str]=None
+
+class STask(STaskAdd):
+    id: int
+
+tasks = []
+
+@app.post("/tasks")
+async def add_task(
+    task: Annotated[STaskAdd, Depends()],
+
+):
+    tasks.append(task)
+    
+    return {"ok": True}
+
+
+# @app.get("/tasks")
+# async def get_tasks():
+#     task = STask(name="Some task")
+#     return {"data": task}
